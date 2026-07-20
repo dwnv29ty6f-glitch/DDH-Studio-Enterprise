1bbcb2a40a1a000e617da43234bdc9fbd1bd52bc
dwnv29ty6f-glitch <dwnv29ty6f@privaterelay.appleid.com>
2026-07-20 21:02:38 +0200

    daten.js aktualisieren

--- js/daten.js
+++ js/daten.js
@@ -1,11 +1,13 @@
-// Hier stehen deine Mitarbeiter
-let mitarbeiter = [
-    { id: 1, name: "Max Mustermann" },
-    { id: 2, name: "Erika Musterfrau" }
+// Globale Daten für DDH Studio Enterprise 9.0
+
+const mitarbeiter = [
+    { id: 1, name: "Max Mustermann", rolle: "Entwickler" },
+    { id: 2, name: "Erika Musterfrau", rolle: "Designerin" },
+    { id: 3, name: "Jan Schmidt", rolle: "Support" } // Neuer Mitarbeiter als Beispiel
 ];
 
-// Hier stehen die Schichten
-let schichten = [
+const schichten = [
     { mitarbeiterId: 1, datum: "2026-07-20", schicht: "Frühschicht" },
-    { mitarbeiterId: 2, datum: "2026-07-20", schicht: "Spätschicht" }
+    { mitarbeiterId: 2, datum: "2026-07-20", schicht: "Spätschicht" },
+    { mitarbeiterId: 3, datum: "2026-07-20", schicht: "Nachtschicht" }
 ];

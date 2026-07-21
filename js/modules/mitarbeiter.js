5ae08f321bdbe0b2cc35a930182dfba369c9ec96
dwnv29ty6f-glitch <dwnv29ty6f@privaterelay.appleid.com>
2026-07-21 16:50:40 +0200

    mitarbeiter.js aktualisieren

--- js/modules/mitarbeiter.js
+++ js/modules/mitarbeiter.js
@@ -57,3 +57,72 @@ function neuePersonalnummer() {
     ) + 1;
 
 }
+/* ==========================================
+   Mitarbeiter hinzufügen
+========================================== */
+
+function mitarbeiterHinzufuegen(daten) {
+
+    const neuerMitarbeiter = {
+
+        id: crypto.randomUUID(),
+
+        personalnummer: neuePersonalnummer(),
+
+        vorname: daten.vorname || "",
+
+        nachname: daten.nachname || "",
+
+        position: daten.position || "",
+
+        abteilung: daten.abteilung || "",
+
+        telefon: daten.telefon || "",
+
+        email: daten.email || "",
+
+        wochenstunden: daten.wochenstunden || 39,
+
+        urlaub: daten.urlaub || 30,
+
+        kranktage: 0,
+
+        eintritt: daten.eintritt || "",
+
+        notizen: daten.notizen || ""
+
+    };
+
+    mitarbeiter.push(neuerMitarbeiter);
+
+    mitarbeiterSpeichern();
+
+    return neuerMitarbeiter;
+
+}
+
+/* ==========================================
+   Mitarbeiter finden
+========================================== */
+
+function mitarbeiterNachId(id) {
+
+    return mitarbeiter.find(m => m.id === id);
+
+}
+
+/* ==========================================
+   Mitarbeiter löschen
+========================================== */
+
+function mitarbeiterLoeschen(id) {
+
+    mitarbeiter = mitarbeiter.filter(
+
+        m => m.id !== id
+
+    );
+
+    mitarbeiterSpeichern();
+
+}

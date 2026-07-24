8796cb43f1ea74d10f6bf41587d81c8d870fd11e
dwnv29ty6f-glitch <dwnv29ty6f@privaterelay.appleid.com>
2026-07-24 19:44:59 +0200

    personalakte.js aktualisieren

--- js/modules/personalakte.js
+++ js/modules/personalakte.js
@@ -56,11 +56,121 @@ const Personalakte = {
 
     </div>
 
-    <div id="personalakteInhalt">
+   <div id="personalakteInhalt">
 
-        Personalakte V2 wird aufgebaut...
+    <div class="personalakteDashboard">
+
+        <div class="personalCard">
+
+            <div class="personalIcon">📅</div>
+
+            <div class="personalTitel">
+
+                Urlaub
+
+            </div>
+
+            <div class="personalWert">
+
+                28 / 30
+
+            </div>
+
+        </div>
+
+        <div class="personalCard">
+
+            <div class="personalIcon">🏥</div>
+
+            <div class="personalTitel">
+
+                Kranktage
+
+            </div>
+
+            <div class="personalWert">
+
+                3
+
+            </div>
+
+        </div>
+
+        <div class="personalCard">
+
+            <div class="personalIcon">⏰</div>
+
+            <div class="personalTitel">
+
+                Stunden
+
+            </div>
+
+            <div class="personalWert">
+
+                ${mitarbeiter.vertragsstunden || 0}
+
+            </div>
+
+        </div>
+
+        <div class="personalCard">
+
+            <div class="personalIcon">📁</div>
+
+            <div class="personalTitel">
+
+                Dokumente
+
+            </div>
+
+            <div class="personalWert">
+
+                0
+
+            </div>
+
+        </div>
+
+        <div class="personalCard">
+
+            <div class="personalIcon">🎓</div>
+
+            <div class="personalTitel">
+
+                Qualifikationen
+
+            </div>
+
+            <div class="personalWert">
+
+                0
+
+            </div>
+
+        </div>
+
+        <div class="personalCard">
+
+            <div class="personalIcon">💰</div>
+
+            <div class="personalTitel">
+
+                Überstunden
+
+            </div>
+
+            <div class="personalWert">
+
+                +0
+
+            </div>
+
+        </div>
 
     </div>
+
+</div>
 
 </div>
 

ebf8e74ebf85076264a7cdbb67f3f8f33b3ace57
dwnv29ty6f-glitch <dwnv29ty6f@privaterelay.appleid.com>
2026-07-24 19:16:56 +0200

    personalakte.js aktualisieren

--- js/modules/personalakte.js
+++ js/modules/personalakte.js
@@ -1,1 +1,72 @@
-j
+const Personalakte = {
+
+    oeffnen(mitarbeiter) {
+
+        Dialog.oeffnen(
+
+            "👤 Personalakte",
+
+            `
+
+<div class="personalakte">
+
+    <h2>
+
+        ${mitarbeiter.vorname} ${mitarbeiter.nachname}
+
+    </h2>
+
+    <p>
+
+        ${mitarbeiter.position || "-"}
+
+    </p>
+
+    <div class="profilTabs">
+
+        <button class="profilTab aktiv">
+
+            👤 Profil
+
+        </button>
+
+        <button class="profilTab">
+
+            💼 Beschäftigung
+
+        </button>
+
+        <button class="profilTab">
+
+            📞 Kontakt
+
+        </button>
+
+        <button class="profilTab">
+
+            📁 Dokumente
+
+        </button>
+
+        <button class="profilTab">
+
+            📝 Notizen
+
+        </button>
+
+    </div>
+
+    <div id="personalakteInhalt">
+
+        Personalakte V2 wird aufgebaut...
+
+    </div>
+
+</div>
+
+`
+        );
+
+    }
+
+};

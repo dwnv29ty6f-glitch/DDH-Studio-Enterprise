03f32cb04b28181606e2fe3480602fb4ade45080
dwnv29ty6f-glitch <dwnv29ty6f@privaterelay.appleid.com>
2026-07-24 20:17:51 +0200

    personalakte.js aktualisieren

--- js/modules/personalakte.js
+++ js/modules/personalakte.js
@@ -1,6 +1,10 @@
 const Personalakte = {
 
+    mitarbeiter: null,
+
     oeffnen(mitarbeiter) {
+
+        this.mitarbeiter = mitarbeiter;
 
         Dialog.oeffnen(
 
@@ -10,109 +14,141 @@ const Personalakte = {
 
 <div class="personalakte">
 
-   <div class="personalHeader">
+    <div class="personalHeader">
 
-    <div class="personalAvatar">
+        <div class="personalAvatar">
 
-        ${mitarbeiter.profilbild
-            ? `<img
-                src="${mitarbeiter.profilbild}"
-                style="
-                    width:100%;
-                    height:100%;
-                    object-fit:cover;
-                    border-radius:50%;
-                ">`
-            : "👤"
-        }
+            ${
+                mitarbeiter.profilbild
 
-    </div>
+                ? `<img
+                        src="${mitarbeiter.profilbild}"
+                        class="personalAvatarBild">`
 
-    <div class="personalInfo">
+                : "👤"
+            }
 
-        <h2>
+        </div>
 
-            ${mitarbeiter.vorname} ${mitarbeiter.nachname}
+        <div class="personalHeaderInfo">
 
-        </h2>
+            <h1>
 
-        <p>
+                ${mitarbeiter.vorname || ""}
 
-            ${mitarbeiter.position || "-"} • ${mitarbeiter.bereich || "-"}
+                ${mitarbeiter.nachname || ""}
 
-        </p>
+            </h1>
 
-        <div class="personalBadges">
+            <div class="personalUntertitel">
 
-            <span class="personalBadge">
+                ${mitarbeiter.position || "-"}
 
-                ${mitarbeiter.status || "Aktiv"}
+                •
 
-            </span>
+                ${mitarbeiter.bereich || "-"}
 
-            <span class="personalBadge">
+            </div>
 
-                📄 ${mitarbeiter.vertragsart || "-"}
+            <div class="personalBadges">
 
-            </span>
+                <span class="personalBadge status">
 
-            <span class="personalBadge">
+                    ${mitarbeiter.status || "Aktiv"}
 
-                ⏰ ${mitarbeiter.vertragsstunden || 0} Std.
+                </span>
 
-            </span>
+                <span class="personalBadge">
+
+                    📄 ${mitarbeiter.vertragsart || "-"}
+
+                </span>
+
+                <span class="personalBadge">
+
+                    ⏰ ${mitarbeiter.vertragsstunden || 0} Std.
+
+                </span>
+
+                <span class="personalBadge">
+
+                    🆔 ${mitarbeiter.personalnummer || "-"}
+
+                </span>
+
+            </div>
 
         </div>
 
     </div>
 
-</div>
+    <div class="profilTabs">
 
-   <div class="profilTabs">
+        <button
 
-    <button class="profilTab aktiv" data-tab="profil">
+            class="profilTab aktiv"
 
-        👤 Profil
+            data-tab="profil">
 
-    </button>
+            👤 Profil
 
-    <button class="profilTab" data-tab="beschaeftigung">
+        </button>
 
-        💼 Beschäftigung
+        <button
 
-    </button>
+            class="profilTab"
 
-    <button class="profilTab" data-tab="kontakt">
+            data-tab="beschaeftigung">
 
-        📞 Kontakt
+            💼 Beschäftigung
 
-    </button>
+        </button>
 
-    <button class="profilTab" data-tab="dokumente">
+        <button
 
-        📁 Dokumente
+            class="profilTab"
 
-    </button>
-
-    <button class="profilTab" data-tab="notizen">
-
-        📝 Notizen
-
-    </button>
-
-</div>
+            data-tab="kontakt">
 
             📞 Kontakt
 
         </button>
 
-        <button class="profilTab">
+        <button
+
+            class="profilTab"
+
+            data-tab="dokumente">
 
             📁 Dokumente
 
         </button>
 
-        <button class="profilTab">
+        <button
+
+            class="profilTab"
+
+            data-tab="qualifikation">
+
+            🎓 Qualifikationen
+
+        </button>
+
+        <button
+
+            class="profilTab"
+
+            data-tab="urlaub">
+
+            📅 Urlaub
+
+        </button>
+
+        <button
+
+            class="profilTab"
+
+            data-tab="notizen">
 
             📝 Notizen
 
@@ -120,127 +156,110 @@ const Personalakte = {
 
     </div>
 
-   <div id="personalakteInhalt">
-
-    <div class="personalakteDashboard">
-
-        <div class="personalCard">
-
-            <div class="personalIcon">📅</div>
-
-            <div class="personalTitel">
-
-                Urlaub
-
-            </div>
-
-            <div class="personalWert">
-
-                28 / 30
-
-            </div>
-
-        </div>
-
-        <div class="personalCard">
-
-            <div class="personalIcon">🏥</div>
-
-            <div class="personalTitel">
-
-                Kranktage
-
-            </div>
-
-            <div class="personalWert">
-
-                3
-
-            </div>
-
-        </div>
-
-        <div class="personalCard">
-
-            <div class="personalIcon">⏰</div>
-
-            <div class="personalTitel">
-
-                Stunden
-
-            </div>
-
-            <div class="personalWert">
-
-                ${mitarbeiter.vertragsstunden || 0}
-
-            </div>
-
-        </div>
-
-        <div class="personalCard">
-
-            <div class="personalIcon">📁</div>
-
-            <div class="personalTitel">
-
-                Dokumente
-
-            </div>
-
-            <div class="personalWert">
-
-                0
-
-            </div>
-
-        </div>
-
-        <div class="personalCard">
-
-            <div class="personalIcon">🎓</div>
-
-            <div class="personalTitel">
-
-                Qualifikationen
-
-            </div>
-
-            <div class="personalWert">
-
-                0
-
-            </div>
-
-        </div>
-
-        <div class="personalCard">
-
-            <div class="personalIcon">💰</div>
-
-            <div class="personalTitel">
-
-                Überstunden
-
-            </div>
-
-            <div class="personalWert">
-
-                +0
-
-            </div>
-
-        </div>
+    <div id="personalContent">
 
     </div>
 
 </div>
 
-</div>
-
 `
+
         );
 
-    }
+        this.events();
 
-};
+        this.tab("profil");
+
+    },
+
+    events() {
+
+        document
+
+            .querySelectorAll(".profilTab")
+
+            .forEach(button => {
+
+                button.onclick = () => {
+
+                    this.tab(
+
+                        button.dataset.tab
+
+                    );
+
+                };
+
+            });
+
+    },
+
+    tab(name) {
+
+        document
+
+            .querySelectorAll(".profilTab")
+
+            .forEach(tab =>
+
+                tab.classList.remove("aktiv")
+
+            );
+
+        document
+
+            .querySelector(
+
+                `[data-tab="${name}"]`
+
+            )
+
+            ?.classList.add("aktiv");
+
+        switch (name) {
+
+            case "profil":
+
+                this.tabProfil();
+
+                break;
+
+            case "beschaeftigung":
+
+                this.tabBeschaeftigung();
+
+                break;
+
+            case "kontakt":
+
+                this.tabKontakt();
+
+                break;
+
+            case "dokumente":
+
+                this.tabDokumente();
+
+                break;
+
+            case "qualifikation":
+
+                this.tabQualifikation();
+
+                break;
+
+            case "urlaub":
+
+                this.tabUrlaub();
+
+                break;
+
+            case "notizen":
+
+                this.tabNotizen();
+
+                break;
+
+        }
+
+    },
